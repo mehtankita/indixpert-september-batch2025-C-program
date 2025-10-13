7050fc0d9a2e11a9597fe119f376128567b938a7
mehtankita <mehtankita392@gmail.com>
2025-10-13 13:42:21 -0700

    change error

--- practic.c
+++ practic.c
@@ -6,22 +6,25 @@ int english;
 int maths;
 int computer;
 int total;
+float percen;
 
-printf("enter marks of hindi");
+printf("enter marks of hindi: ");
 scanf("%d",&hindi);
 
-printf ("enter marks of english");
+printf ("enter marks of english: ");
 scanf("%d",&english);
 
-printf ("enter marks of maths");
+printf ("enter marks of maths: ");
 scanf("%d",&maths);
 
-printf ("enter marks of computer");
+printf ("enter marks of computer: ");
 scanf("%d",&computer);
 
 total=hindi+english+maths+computer;
+percen=total/4;
 
 printf("total marks is %d",total);
+printf("percen is : %f",percen);
 
     return 0;
 }
